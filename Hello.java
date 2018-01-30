6966eab88297040abb199eee1d85f7ac9eecade5
ldy <tlgurdlsn37@naver.com>
2018-01-30 11:54:57 +0900

    added output "Tell Her World"

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ package git_test
 public class HelloGit{
 	public static void main(String[] args){
 		System.out.println("Hellow Git");
-	System.out.println("Tell Your World")
+		System.out.println("Tell Your World");
+		System.out.println("Tell Her World");
 	}
